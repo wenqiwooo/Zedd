f5406a80c1f05f56caed0e15b36400166d4c02ba
annabeltay17 <annabel.tay17@gmail.com>
2016-01-24 03:39:37 +0800

    Update telegram.py
    Added commands to turn on different types of light  and to change the brightness of the lights

--- telegram.py
+++ telegram.py
@@ -35,6 +35,9 @@ def handle(msg):
 	elif command == '/romantic':
 		moodCtlr.setMoodRomantic(lightCtlr, mPlayer)
 		bot.sendMessage(chat_id, 'Setting mood to romantic...')
+	elif command == '/productivity':
+		moodCtlr.setMoodProductivity(lightCtlr, mPlayer)
+		bot.sendMessage(chat_id, 'Setting mood to productivity...')
 	elif command == '/glance':
 		f = camCtlr.getPicture()
 		bot.sendPhoto(chat_id, f)
@@ -44,7 +47,46 @@ def handle(msg):
                         bot.sendMessage(chat_id, 'No significant change detected...')
                 else:
                         bot.sendPhoto(chat_id, f)
-                        
+        elif command == '/whiteLight':
+        	lightCtlr.on(0)
+	elif command == '/orangeLight':
+		lightCtlr.on(1)
+	elif command == '/coolLight':
+		lightCtlr.on(2)
+	elif command == '/blackLight':
+		lightCtlr.on(3)
+	elif command == '/brightOne':
+		lightCtlr.setBrightness(1)
+	elif command == '/brightTwo':
+		lightCtlr.setBrightness(2)
+	elif command == '/brightThree':
+		lightCtlr.setBrightness(3)
+	elif command == '/brightFour':
+		lightCtlr.setBrightness(4)
+	elif command == '/help':
+		a = 'To turn on light:' + 
+		    '/whiteLight to turn on white light' +
+		    '/orangeLight to turn on orange light' + 
+		    '/coolLight to turn on cool ambience light' +
+		    '/blackLight to turn on black light' + ' ' +
+		    
+		    'To change brightness of light:' +
+		    '/brightOne for the the dimmest setting' +
+		    '/brightFour for the brightest setting' + ' ' +
+		    
+		    'Music Commands:' + 
+		    '/playmusic to start playng music' + 
+		    '/stopmusic to stop playing music' +
+		    '/pausemusic to pause the current music' + 
+		    '/nextsong to skip the current music' + ' ' + 
+		    
+		    'Mood Commands:' + 
+		    '/productivity to set conducive music and lighting' +
+		    '/party to set party lights and music' + 
+		    '/romantic to set romantic lights and music'
+		    
+		bot.sendMessage(chat_id, a)
+	
 # Initialize MusicPlayer
 mPlayer = MusicPlayer()
 
